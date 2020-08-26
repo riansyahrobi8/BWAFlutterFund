c5ec43cd61dc3166011cfaa1ec08157979f13a95
riansyahrobi8 <robidahariansyah8@gmail.com>
2020-08-26 16:04:24 +0700

    ubah tampilan

--- latihan_part1/lib/main.dart
+++ latihan_part1/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -8,6 +10,23 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  Random valueRandom = Random();
+  List<Widget> widgets = [];
+
+  _MyAppState() {
+    for (int i = 0; i < 15; i++) {
+      widgets.add(Container(
+        margin: const EdgeInsets.only(left: 24.0, top: 24.0, right: 24.0),
+        width: 50.0 + valueRandom.nextInt(251),
+        height: 52.0,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12.0),
+            color: Color.fromARGB(255, valueRandom.nextInt(256),
+                valueRandom.nextInt(256), valueRandom.nextInt(256))),
+      ));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -20,8 +39,7 @@ class _MyAppState extends State<MyApp> {
           ),
           body: Padding(
             padding: const EdgeInsets.all(24.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+            child: ListView(
               children: [
                 Text(
                   "Selamat Datang, Hooman...",
@@ -37,6 +55,45 @@ class _MyAppState extends State<MyApp> {
                   "Silahkan klik container untuk ubah warna dan ukuran panjang!",
                   style: TextStyle(
                       fontSize: 15, fontFamily: "Roboto", color: Colors.grey),
+                ),
+                SizedBox(
+                  height: 48.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    RaisedButton.icon(
+                        onPressed: () {},
+                        color: Color(0xFFFF7D7D),
+                        icon: Icon(
+                          Icons.delete,
+                          color: Colors.white,
+                        ),
+                        label: Text(
+                          "Hapus",
+                          style: TextStyle(
+                              fontFamily: "Roboto", color: Colors.white),
+                        )),
+                    RaisedButton.icon(
+                        onPressed: () {},
+                        color: Color(0xFF74F45F),
+                        icon: Icon(
+                          Icons.add,
+                          color: Colors.white,
+                        ),
+                        label: Text(
+                          "Tambah",
+                          style: TextStyle(
+                              fontFamily: "Roboto", color: Colors.white),
+                        )),
+                  ],
+                ),
+                SizedBox(
+                  height: 24.0,
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: widgets,
                 )
               ],
             ),
