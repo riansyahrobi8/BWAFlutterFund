312c3faf27301b4384a7bb360f4369047cf2d4ea
riansyahrobi8 <robidahariansyah8@gmail.com>
2020-08-28 00:00:13 +0700

    edit CustomColor.dart, ada image ss and edit README.md

--- latihan_part1/lib/utils/CustomColor.dart
+++ latihan_part1/lib/utils/CustomColor.dart
@@ -1,24 +1,3 @@
-// enum MyColor { appBarColor, delButtonColor, addButtonColor, black, white, body }
-
-// extension SelectedColor on MyColor {
-//   int get color {
-//     switch (this) {
-//       case MyColor.appBarColor:
-//         return 0xFFFE7F49;
-//       case MyColor.delButtonColor:
-//         return 0xFFFF7D7D;
-//       case MyColor.addButtonColor:
-//         return 0xFF74F45F;
-//       case MyColor.white:
-//         return 0xFFFFFFFF;
-//       case MyColor.body:
-//         return 0xFFFAFAFA;
-//       default:
-//         return 0xFF000000;
-//     }
-//   }
-// }
-
 import 'package:flutter/material.dart';
 
 const appBarColor1 = const Color(0xFFFE7F49);
