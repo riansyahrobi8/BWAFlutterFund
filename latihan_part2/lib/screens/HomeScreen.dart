19e8880278982beb0fed31c59d1b467e885e969e
riansyahrobi8 <robidahariansyah8@gmail.com>
2020-09-05 20:59:14 +0700

    load data from local json

--- latihan_part2/lib/screens/HomeScreen.dart
+++ latihan_part2/lib/screens/HomeScreen.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:latihan_part2/utils/CustomColor.dart' as myColor;
 
@@ -7,6 +9,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List data;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,6 +22,26 @@ class _HomeScreenState extends State<HomeScreen> {
             style: TextStyle(color: myColor.titleColor, fontFamily: "Roboto"),
           ),
         ),
+      ),
+      body: FutureBuilder(
+        builder: (context, snapshot) {
+          var places = json.decode(snapshot.data.toString());
+
+          return ListView.builder(
+            itemBuilder: (BuildContext context, int index) {
+              return Card(
+                child: Column(
+                  children: [
+                    Image(image: AssetImage("images/" + places[index]["image"]))
+                  ],
+                ),
+              );
+            },
+            itemCount: places.length,
+          );
+        },
+        future: DefaultAssetBundle.of(context)
+            .loadString("data/galeri_indonesia.json"),
       ),
     );
   }
