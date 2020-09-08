f9309f3c1ae0e91539892aae23a0493ead13491a
riansyahrobi8 <robidahariansyah8@gmail.com>
2020-09-08 11:12:47 +0700

    edit detail screen

--- latihan_part2/lib/screens/DetailScreen.dart
+++ latihan_part2/lib/screens/DetailScreen.dart
@@ -8,6 +8,8 @@ class DetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
     return Scaffold(
         appBar: AppBar(
           backgroundColor: myColor.mainColor,
@@ -15,17 +17,75 @@ class DetailScreen extends StatelessWidget {
         ),
         body: Stack(
           children: [
+            Image(
+              image: AssetImage("images/" + image),
+              fit: BoxFit.cover,
+              width: double.infinity,
+              height: double.infinity,
+            ),
             Container(
-                width: double.infinity,
-                height: double.infinity,
-                child: Image(
-                  image: AssetImage("images/" + image),
-                  fit: BoxFit.cover,
-                )),
-            Text(
-              placeName,
-              style: TextStyle(color: myColor.titleColor, fontFamily: "Oswald"),
-            )
+              width: double.infinity,
+              height: double.infinity,
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(colors: <Color>[
+                Color.fromRGBO(0, 0, 0, 0.0),
+                Color.fromRGBO(0, 0, 0, .1),
+                Color.fromRGBO(0, 0, 0, .2),
+                Color.fromRGBO(0, 0, 0, .3),
+                Color.fromRGBO(0, 0, 0, .4),
+                Colors.black
+              ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+            ),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Container(
+                margin: const EdgeInsets.only(
+                    left: 16.0, right: 16.0, bottom: 56.0),
+                child: Text(
+                  placeName,
+                  style: TextStyle(
+                      color: myColor.titleColor,
+                      fontFamily: "Oswald",
+                      fontSize: 20.0),
+                ),
+              ),
+            ),
+            DraggableScrollableSheet(
+                initialChildSize: 0.075,
+                minChildSize: 0.075,
+                maxChildSize: 1,
+                builder:
+                    (BuildContext context, ScrollController scrollController) {
+                  return SingleChildScrollView(
+                    controller: scrollController,
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: myColor.titleColor,
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(24.0),
+                              topRight: Radius.circular(24.0))),
+                      width: size.width,
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              "Photo by $name on Unsplash.com",
+                              style: TextStyle(
+                                  fontFamily: "Roboto",
+                                  color: myColor.mainColor),
+                            ),
+                            SizedBox(
+                              height: 12.0,
+                            ),
+                            Text(account)
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                })
           ],
         ));
   }
