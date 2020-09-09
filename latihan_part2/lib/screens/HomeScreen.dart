eb4e12a8c05ad562a6d92205b98c21dbd1793d15
riansyahrobi8 <robidahariansyah8@gmail.com>
2020-09-09 23:58:30 +0700

    load data json using future async in background

--- latihan_part2/lib/screens/HomeScreen.dart
+++ latihan_part2/lib/screens/HomeScreen.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:latihan_part2/screens/DetailScreen.dart';
 import 'package:latihan_part2/utils/CustomColor.dart' as myColor;
+import 'package:flutter/services.dart' show rootBundle;
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -12,10 +13,17 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   List data;
 
+  Future<String> loadData() async {
+    var jsonFile = await rootBundle.loadString("data/galeri_indonesia.json");
+    setState(() {
+      data = json.decode(jsonFile);
+    });
+  }
+
   @override
   void initState() {
     super.initState();
-    data = [];
+    this.loadData();
   }
 
   @override
@@ -40,6 +48,16 @@ class _HomeScreenState extends State<HomeScreen> {
       body: ListView(
         children: [
           Container(
+            child: ListView.builder(
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (BuildContext context, int index) => Container(
+                      width: 100.0,
+                      height: 100.0,
+                      color: Colors.amber,
+                    )),
+          ),
+          Container(
             margin: const EdgeInsets.symmetric(horizontal: 16.0),
             child: Text(
               "Terbaru",
@@ -53,47 +71,40 @@ class _HomeScreenState extends State<HomeScreen> {
           SizedBox(
             height: 8.0,
           ),
-          FutureBuilder(
-            future: DefaultAssetBundle.of(context)
-                .loadString("data/galeri_indonesia.json"),
-            builder: (context, snapshot) {
-              var places = json.decode(snapshot.data.toString());
-              return GridView.builder(
-                  shrinkWrap: true,
-                  itemCount: places == null ? 0 : places.length,
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                      childAspectRatio: size.width / (size.height / 1.25)),
-                  padding: const EdgeInsets.all(16.0),
-                  itemBuilder: (context, index) {
-                    return GestureDetector(
-                      onTap: () {
-                        Navigator.of(context)
-                            .push(MaterialPageRoute(builder: (context) {
-                          return DetailScreen(
-                            name: places[index]['name'],
-                            account: places[index]['account'],
-                            image: places[index]['image'],
-                            placeName: places[index]['place_name'],
-                          );
-                        }));
-                      },
-                      child: Card(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(12.0)),
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(12.0),
-                          child: Image(
-                            image:
-                                AssetImage("images/" + places[index]["image"]),
-                            fit: BoxFit.cover,
-                          ),
-                        ),
+          GridView.builder(
+              shrinkWrap: true,
+              physics: ScrollPhysics(),
+              itemCount: data == null ? 0 : data.length,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio: size.width / (size.height / 1.25)),
+              padding: const EdgeInsets.all(16.0),
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.of(context)
+                        .push(MaterialPageRoute(builder: (context) {
+                      return DetailScreen(
+                        name: data[index]['name'],
+                        account: data[index]['account'],
+                        image: data[index]['image'],
+                        placeName: data[index]['place_name'],
+                      );
+                    }));
+                  },
+                  child: Card(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12.0)),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(12.0),
+                      child: Image(
+                        image: AssetImage("images/" + data[index]["image"]),
+                        fit: BoxFit.cover,
                       ),
-                    );
-                  });
-            },
-          ),
+                    ),
+                  ),
+                );
+              })
         ],
       ),
     );
